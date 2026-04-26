7e9a7c2a95939e42af61559990d1132ee9f0ad24
anaghaavinashcs24 <anaghaavinash.cs24@bmsce.ac.in>
2026-04-26 22:07:48 +0530

    Add files via upload

---
+++ revogue-app/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Revogue from './Revogue.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Revogue />
+  </React.StrictMode>
+);
